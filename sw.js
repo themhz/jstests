d5c94b32271d144db95607e40429d5bea8a338c4
themhz <themhz@gmail.com>
2023-10-11 16:07:19 +0300

    working with service workers

--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
 self.addEventListener('push', function(event) {
     console.log('Received push event:', event);
+    console.log('message data:',event.data.text());
+    console.log('message data text ', event.data);
+    
     const title = 'New Notification';
     const options = {
         body: event.data.text(),
     };
     event.waitUntil(self.registration.showNotification(title, options));
+
+    self.clients.matchAll().then(clients => {
+        clients.forEach(client => client.postMessage({
+            type: 'PUSH_NOTIFICATION',
+            data: event.data.text()
+        }));
+    });
 });
+
+//http://127.0.0.1:5501/index.html
